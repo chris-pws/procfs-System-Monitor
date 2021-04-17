cd8c5b13dd44a92c6460a500986c66a2855ce104
Chris <cms@dreaming.org>
2021-04-17 00:57:01 -0400

    Supplied NCursesDisplay now supports multiple CPUs; added check for NaN result in Processor::UpdateResult(); added Processor::NumCpus() helper for NCursesDisplay::Display() window geometry

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -32,13 +32,22 @@ std::string NCursesDisplay::ProgressBar(float percent) {
 
 void NCursesDisplay::DisplaySystem(System &system, WINDOW *window) {
     int row{0};
+    int cpu_id{0};
     mvwprintw(window, ++row, 2, ("OS: " + system.OperatingSystem()).c_str());
     mvwprintw(window, ++row, 2, ("Kernel: " + system.Kernel()).c_str());
-    mvwprintw(window, ++row, 2, "CPU: ");
-    wattron(window, COLOR_PAIR(1));
-    mvwprintw(window, row, 10, "");
-    // wprintw(window, ProgressBar(system.Cpu().Utilization()).c_str());
-    wattroff(window, COLOR_PAIR(1));
+
+    for (float cpu : system.Cpu().Utilization()) {
+        mvwprintw(
+            window, ++row, 2,
+            std::string(std::string("CPU") + std::to_string(cpu_id) + ": ")
+                .c_str());
+        wattron(window, COLOR_PAIR(1));
+        mvwprintw(window, row, 10, "");
+        wprintw(window, ProgressBar(cpu).c_str());
+        wattroff(window, COLOR_PAIR(1));
+        cpu_id++;
+    }
+
     mvwprintw(window, ++row, 2, "Memory: ");
     wattron(window, COLOR_PAIR(1));
     mvwprintw(window, row, 10, "");
@@ -93,7 +102,7 @@ void NCursesDisplay::Display(System &system, int n) {
     start_color();  // enable color
 
     int x_max{getmaxx(stdscr)};
-    WINDOW *system_window = newwin(9, x_max - 1, 0, 0);
+    WINDOW *system_window = newwin(8 + system.Cpu().NumCpus(), x_max - 1, 0, 0);
     WINDOW *process_window =
         newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
 
